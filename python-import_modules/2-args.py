b45c6fbe9590352b87493713366a19bf861b724c
fayez bast <fayezbast15@gmai.com>
2025-03-21 20:34:16 +0000

    fixed minor issue

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 import sys
 
-def main():
-    argv = sys.argv[1:]  # Exclude the script name itself
-    num_args = len(argv)
-    
-    if num_args == 0:
-        print("0 arguments.")
-    else:
-        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
-        for i, arg in enumerate(argv, start=1):
-            print(f"{i}: {arg}")
+if __name__ != "__main__":
+    exit()
 
-if __name__ == "__main__":
-    main()
+argStr = "{:d} argument"
+argc = len(sys.argv) - 1
+if argc == 0:
+    argStr += 's.'
+elif argc == 1:
+    argStr += ':'
+else:
+    argStr += 's:'
+print(argStr.format(argc))
 
+i = 0
+for arg in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, arg))
+    i += 1
